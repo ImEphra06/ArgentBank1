42a937eb6bda503601333dbff17e488f0a99f6c2
ImEphra06 <bart94620@hotmail.fr>
2025-02-17 00:07:59 +0100

    troubleshoot in react version

---
+++ src/components/header/header.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { userLogout } from "../../features/userSlice";
+import { useDispatch } from "react-redux";
+import logo from "../../asset/argentBankLogo.png";
+import "../../style/main.css";
+
+function Header() {
+	const dispatch = useDispatch();
+
+	// retrieve store data to know about user connexion
+	const { firstName } = useSelector((state) => state.user);
+	const connected = sessionStorage.getItem("connected");
+
+	function Signout() {
+		dispatch(userLogout());
+	}
+
+	// Header actions depend on user connection
+	function headerActions() {
+		return connected ? (
+			<div>
+				<NavLink to="/profile" className="main-nav-item">
+					<i className="fa fa-user-circle" />
+					{` ${firstName}`}
+				</NavLink>
+				<NavLink to="/" className="main-nav-item" onClick={Signout}>
+					<i className="fa fa-sign-out" /> Sign Out
+				</NavLink>
+			</div>
+		) : (
+			<div>
+				<NavLink to="/login" className="main-nav-item">
+					<i className="fa fa-user-circle" /> Sign In
+				</NavLink>
+			</div>
+		);
+	}
+
+	return (
+		<nav className="main-nav">
+			<NavLink to="/" className="main-nav-logo">
+				<img className="main-nav-logo-image" src={logo} alt="Argent Bank Logo" />
+				<h1 className="sr-only">Argent Bank</h1>
+			</NavLink>
+			{headerActions()}
+		</nav>
+	);
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import './index.css';
-import App from './App';
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Index from "./page/index/index";
+//import SignIn from "./page/signin/signin";
+//import User from "./page/user/user";
+import ErrorPage from "./page/error/error";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+    <Header />
+      <Routes>
+      <Route path="/" element={<Index />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </Router>
   </React.StrictMode>
 );
 
